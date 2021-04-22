a36afc2d16a6dc8e217fd032ddbfce6f062e1567
Jerry Koers <jerry@verploegen.nl>
2021-04-22 17:11:35 +0200

    test 5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // insert your signing secret in the .env file
 const signingSecret = process.env.SIGNING_SECRET || '';
 
-if(!signingSecret || signingSecret === 'your secret here') {
+if(!signingSecret || signingSecret === 'V3rpl03g3n637') {
   console.warn('No signing secret defined! You can define the SIGNING_SECRET in your Heroku settings: https://devcenter.heroku.com/articles/config-vars#using-the-heroku-dashboard');
 }
 
